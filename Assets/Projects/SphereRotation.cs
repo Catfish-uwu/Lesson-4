9f164ed8b18dbedc77fbe1110f6c7ff3ea835a89
Vbmir5 <dasha-mir4@mail.ru>
2019-10-15 19:36:29 +0300

    Project

---
+++ Assets/Projects/SphereRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereRotation : MonoBehaviour
+{
+    [Header("Вокруг чего происходит вращение")]
+    public Transform planetToRotate;
+    [Header("Скорость вращения (градусы в секунду)")]
+    public float speed = 0;
+
+    void Update()
+    {
+        gameObject.transform.RotateAround(planetToRotate.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
